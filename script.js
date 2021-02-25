016d10a4afeffa02b2f9111c393037659cbeb024
e-gunn <gunn816@hotmail.com>
2021-02-24 20:47:11 -0800

    Update project design theme

--- script.js
+++ script.js
@@ -18,31 +18,31 @@ return politician;
 
 };
 
-var liz = createPolitician("Elizabeth Warren", [132, 17, 11]);
-var katie = createPolitician("Katie Porter", [245, 141, 136]);
+var shalane = createPolitician("Shalane Flanagan", [170, 6, 6]);
+var desiree = createPolitician("Desiree Linden", [212, 175, 55]);
 
-liz.electionResults = [5, 1, 7, 2, 33, 6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 3, 7, 4, 8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 10, 3];
+shalane.electionResults = [5, 1, 7, 2, 33, 6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 3, 7, 4, 8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 10, 3];
 
-katie.electionResults = [4, 2, 4, 4, 22, 3, 3, 1, 2, 15, 8, 1, 3, 9, 0, 6, 1, 5, 5, 1, 3, 7, 8, 1, 3, 3, 1, 3, 2, 2, 6, 2, 14, 0, 1, 6, 7, 3, 7, 3, 6, 1, 3, 17, 3, 1, 2, 11, 2, 3, 1];
+desiree.electionResults = [4, 2, 4, 4, 22, 3, 3, 1, 2, 15, 8, 1, 3, 9, 0, 6, 1, 5, 5, 1, 3, 7, 8, 1, 3, 3, 1, 3, 2, 2, 6, 2, 14, 0, 1, 6, 7, 3, 7, 3, 6, 1, 3, 17, 3, 1, 2, 11, 2, 3, 1];
 
-liz.electionResults[9] = 1;
-katie.electionResults[9] = 28;
+shalane.electionResults[9] = 1;
+desiree.electionResults[9] = 28;
 
-liz.electionResults[4] = 17;
-katie.electionResults[4] = 38;
+shalane.electionResults[4] = 17;
+desiree.electionResults[4] = 38;
 
-liz.electionResults[43] = 11;
-katie.electionResults[43] = 27;
+shalane.electionResults[43] = 11;
+desiree.electionResults[43] = 27;
 
-console.log(liz.electionResults);
-console.log(katie.electionResults);
+console.log(shalane.electionResults);
+console.log(desiree.electionResults);
 
 var setStateResults = function(state) {
   theStates[state].winner = null;
-  if (liz.electionResults[state] > katie.electionResults[state]) {
-    theStates[state].winner = liz;
-  } else if (liz.electionResults[state] < katie.electionResults[state]) {
-    theStates[state].winner = katie;
+  if (shalane.electionResults[state] > desiree.electionResults[state]) {
+    theStates[state].winner = shalane;
+  } else if (shalane.electionResults[state] < desiree.electionResults[state]) {
+    theStates[state].winner = desiree;
   }
 
   var stateWinner = theStates[state].winner;
@@ -50,37 +50,37 @@ var setStateResults = function(state) {
   if (stateWinner !== null) {
     theStates[state].rgbColor = stateWinner.partyColor;
   } else {
-    theStates[state].rgbColor = [11, 32, 57];
+    theStates[state].rgbColor = [0, 0, 0];
   }
 
-liz.tallyUpTotalVotes();
-katie.tallyUpTotalVotes();
+shalane.tallyUpTotalVotes();
+desiree.tallyUpTotalVotes();
 
-console.log(liz.totalVotes);
-console.log(katie.totalVotes);
+console.log(shalane.totalVotes);
+console.log(desiree.totalVotes);
 
 var winner = null;
 
-  if (liz.totalVotes > katie.totalVotes) {
-     winner = liz.name;
-  } else if (liz.totalVotes < katie.totalVotes) {
-     winner = katie.name;
+  if (shalane.totalVotes > desiree.totalVotes) {
+     winner = shalane.name;
+  } else if (shalane.totalVotes < desiree.totalVotes) {
+     winner = desiree.name;
   } else {
      winner = "DRAW";
   }
 
 console.log("And the winner is... " + winner +"!");
 
-console.log("Elizabeth's color is: " + liz.partyColor);
-console.log("Katie's color is: " + katie.partyColor);
+console.log("Shalane's color is: " + shalane.partyColor);
+console.log("Desiree's color is: " + desiree.partyColor);
 
 var countryInfoTable = document.getElementById("countryResults");
 var row = countryInfoTable.children[0].children[0];
 
-row.children[0].innerText = liz.name;
-row.children[1].innerText = liz.totalVotes;
-row.children[2].innerText = katie.name;
-row.children[3].innerText = katie.totalVotes;
+row.children[0].innerText = shalane.name;
+row.children[1].innerText = shalane.totalVotes;
+row.children[2].innerText = desiree.name;
+row.children[3].innerText = desiree.totalVotes;
 row.children[5].innerText = winner;
 
 var stateInfoTable = document.getElementById("stateResults");
@@ -97,11 +97,11 @@ var winnersName = body.children[2].children[1];
 stateName.innerText = theStates[state].nameFull;
 abbrev.innerText = "(" + theStates[state].nameAbbrev + ")";
 
-candidate1Name.innerText = liz.name;
-candidate2Name.innerText = katie.name;
+candidate1Name.innerText = shalane.name;
+candidate2Name.innerText = desiree.name;
 
-candidate1Results.innerText = liz.electionResults[state];
-candidate2Results.innerText = katie.electionResults[state];
+candidate1Results.innerText = shalane.electionResults[state];
+candidate2Results.innerText = desiree.electionResults[state];
 
   if (theStates[state].winner === null) {
     winnersName.innerText = "DRAW";
